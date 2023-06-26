7feef0b45617308661beec8947efbd1a391c16cb
nikita <paramonov20@bk.ru>
2023-06-26 21:37:29 +0300

    Начал узучение BroadcastReceiver, создал свой первый receiver

--- app/src/main/java/ru/paramonov/brodcast/MainActivity.kt
+++ app/src/main/java/ru/paramonov/brodcast/MainActivity.kt
@@ -2,13 +2,25 @@ package ru.paramonov.brodcast
 
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = MyReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val airPlaneMode = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        val lowBattery = IntentFilter(Intent.ACTION_BATTERY_LOW)
+        registerReceiver(receiver, airPlaneMode)
+        registerReceiver(receiver, lowBattery)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }

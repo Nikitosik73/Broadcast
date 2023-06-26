375d76d30273425122022e3c6a032550de131ee4
nikita <paramonov20@bk.ru>
2023-06-26 21:30:35 +0300

    create project

---
+++ app/src/main/java/ru/paramonov/brodcast/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.paramonov.brodcast
+
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
